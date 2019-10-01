fd1affdede31fbbf05faec654f58444172165b5e
Jennifer E. Yoon <jenneyoon@gmail.com>
2019-10-01 16:47:34 -0400

    Update numpy-named-copy.py

--- wip1/so/numpy-named-copy.py
+++ wip1/so/numpy-named-copy.py
@@ -11,9 +11,8 @@ def myfunc(arr):
                 # Create a new array name from string parts.
                 name = "arr"+str(i)+str(j)+str(k)
                 print(name, b[i, j, k])
-                # Create a dictionary to save all the parts.
+                # Add a new key-value pair to the dictionary.
                 mydict.update({name: b[i,j,k]})
-                
     return(mydict)
 
 mydict = {}
@@ -23,3 +22,12 @@ print(result)
 # You would need to unpack the dictionary to use the arrays separately.
 # use "mydict.keys()" to get all array names.
 # "for key in keys" to loop through all array names. 
+# mydict['arr000'] will return the value 0.
+
+"""
+Your question tags "numpy" but does not use it in your code snippet. If you are trying to stick with numpy, there is another method called "structured data array". It's similar to a dictionary in that "name" and "value" can be stored as paired sets in a numpy array. This keeps numpy's efficient memory management and fast calculation (C optimization). This matters if you are working with large datasets.
+
+Also if working with numpy, there may be a way to use its index attribute in a variable name.
+
+Later, I will think of examples for both and update my answer if possible.
+"""
