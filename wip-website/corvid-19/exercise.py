d4006567dd48b97bbcb4de287e503eae0ace067e
Jennifer E. Yoon <jenneyoon@gmail.com>
2020-05-07 16:59:02 -0400

    Update exercise.py

--- wip-website/corvid-19/exercise.py
+++ wip-website/corvid-19/exercise.py
@@ -1,3 +1,18 @@
 # My python github short exercise.  
 # Date:  May 7, 2020  
 
+# Imports: standard, third party, my custom, in alphabetical order.  
+
+# built-in modules 
+import datetime as dt  
+
+# third party modules
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt  
+import seaborn as sns  
+
+# my modules  
+import myfunc  
+import myutils  
+
