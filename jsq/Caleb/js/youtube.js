5c5994d43993bba6790ccbe2b570641f31afcf75
JennEYoon <jenneyoon@gmail.com>
2021-02-06 02:43:36 -0500

    Add Caleb js

---
+++ jsq/Caleb/js/youtube.js
@@ -0,0 +1,12 @@
+var username = prompt();
+// alert(username);
+console.log(username);
+
+/* document.getElementById("button").onclick = function(){
+    document.getElementById("confirm").innerHTML = "Order Placed. Check your email.";
+    document.getElementById("confirm").style.backgroundColor = "yellow";
+    document.getElementById("button").style.display = "none";
+}; */
+
+/* alert("Hello world!");
+alert("Nice to see you too."); */
