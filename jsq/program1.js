9fc1062b1f13462935220fa4ea3272d805d50e94
JennEYoon <jenneyoon@gmail.com>
2021-04-02 17:19:17 -0400

    Move jsq to jennifer-yoon repo

--- jsq/program1.js
+++
@@ -1,6 +0,0 @@
-// program1.js -- test file. JY
-
-document.writeln("Hello World!  JY");
-document.write("Write does not add new line."); //comment in-line.
-/* This is a block comment. But some escape characters
-may also have this symbols, so use sparingly. */
